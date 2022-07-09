01b43d3089cdbede5e4bb619930a5d1ab78107de
Julius <Julius.leeba@absa.africa>
2022-07-09 22:34:03 +0200

    User GQLModel

---
+++ server/GQL/GQLModels.js
@@ -0,0 +1,13 @@
+const { GraphQLString, GraphQLObjectType, GraphQLNonNull, GraphQLFloat } = require('graphql');
+
+const UserModel = new GraphQLObjectType({
+    name: 'User'
+    ,
+    fields: () => ({
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        height: { type: new GraphQLNonNull(GraphQLFloat) }
+    })
+});
+
+module.exports = { UserModel }
--- server/server.js
+++ server/server.js
@@ -3,12 +3,23 @@ const app = express();
 const dotEnv = require('dotenv');
 const cors = require('cors');
 dotEnv.config();
+const { buildSchema} = require('graphql');
+const { graphqlHTTP} = require('express-graphql');
 
 const PORT = process.env.PORT;
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
 app.use(cors());
 
-app.use()
+app.use('api/grapql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
